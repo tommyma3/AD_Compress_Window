b4496c893a9f4901f4c154c83ef4b18bfa676dbc
CutiRomero02 <124090467@link.cuhk.edu.cn>
2025-11-17 13:14:43 +0800

    introduce dataset compress technique

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -1,5 +1,6 @@
 from .ppo import PPOWrapper
 from .utils import HistoryLoggerCallback
+from .compressed_history_callback import CompressedHistoryLoggerCallback
 
 ALGORITHM = {
     'PPO': PPOWrapper,
