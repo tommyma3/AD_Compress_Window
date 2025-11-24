f2e98f9c7fbd52eb76cf70c3e98d4b987f165e42
CutiRomero02 <124090467@link.cuhk.edu.cn>
2025-11-24 15:31:59 +0800

    init

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -1,6 +1,5 @@
 from .ppo import PPOWrapper
 from .utils import HistoryLoggerCallback
-from .compressed_history_callback import CompressedHistoryLoggerCallback
 
 ALGORITHM = {
     'PPO': PPOWrapper,
--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,5 @@
 from .ad import AD
-from .compressed_ad import CompressedAD
 
 MODEL = {
-    "AD": AD,
-    "CompressedAD": CompressedAD
+    "AD": AD
 }
